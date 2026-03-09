058156fa24bb132ccb1daca80a428e46dd53953b
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2026-03-09 04:48:14 -0500

    feat: add password encryption using bcrypt

--- main.go
+++ main.go
@@ -11,37 +11,44 @@ import (
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
-// User define la estructura para DynamoDB
 type User struct {
 	UUID     string `json:"uuid" dynamodbav:"uuid"`
 	Name     string `json:"name" dynamodbav:"name"`
 	LastName string `json:"lastName" dynamodbav:"lastName"`
 	Email    string `json:"email" dynamodbav:"email"`
-	Password string `json:"password" dynamodbav:"password"` // Pendiente: Encriptar
+	Password string `json:"password" dynamodbav:"password"`
 	Document string `json:"document" dynamodbav:"document"`
 }
 
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
 func HandleRequest(ctx context.Context, user User) (string, error) {
-	// 1. Cargar configuración (Conectará a LocalStack por las variables de entorno)
 	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		return "Error de configuración", err
+		return "Error config", err
 	}
 
 	dbClient := dynamodb.NewFromConfig(cfg)
 
-	// Generar UUID si no viene
+	hashedPass, err := hashPassword(user.Password)
+	if err != nil {
+		return "Error encriptando", err
+	}
+	user.Password = hashedPass
+
 	user.UUID = uuid.New().String()
 
-	// 2. Mapear objeto a formato DynamoDB
 	item, err := attributevalue.MarshalMap(user)
 	if err != nil {
-		return "Error mapeando datos", err
+		return "Error mapeo", err
 	}
 
-	// 3. Guardar en la tabla
 	_, err = dbClient.PutItem(ctx, &dynamodb.PutItemInput{
 		TableName: aws.String("pigbank-users"),
 		Item:      item,
@@ -49,10 +56,10 @@ func HandleRequest(ctx context.Context, user User) (string, error) {
 
 	if err != nil {
 		log.Printf("Error DB: %v", err)
-		return "Error guardando en base de datos", err
+		return "Error DB", err
 	}
 
-	return fmt.Sprintf("Usuario %s registrado con ID %s", user.Name, user.UUID), nil
+	return fmt.Sprintf("Usuario %s registrado y protegido", user.UUID), nil
 }
 
 func main() {
